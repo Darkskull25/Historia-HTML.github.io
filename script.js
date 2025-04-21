4c4e2cd20bc2cd6f1b49ab287fdb9ef2d7479ee9
Darkskull25 <estebanjuan0000@gmail.com>
2025-04-20 19:14:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-<script>
+ <script>
         function cambiarTema() {
             document.body.classList.toggle("tema-claro");
             document.body.classList.toggle("tema-oscuro");
         }
 
-  document.addEventListener("DOMContentLoaded" , function () {
-       const musica = document.getElementById("musicaFondo");
-       const boton = document.getElementById("muteBtn");
+        document.addEventListener("DOMContentLoaded", function () {
+            const musica = document.getElementById("musicaFondo");
+            const boton = document.getElementById("muteBtn");
 
-      function iniciarAudio() {
-      musica.play().catch(() => {}); 
-      document.removeEventListener("mousemove", iniciarAudio);
-        }
+            function iniciarAudio() {
+                musica.play().catch(() => {});
+                document.removeEventListener("mousemove", iniciarAudio);
+            }
 
-      document.addEventListener("mousemove", iniciarAudio);
+            document.addEventListener("mousemove", iniciarAudio);
 
-      boton.addEventListener("click", funcion () {
-          if (musica.paused) {
-              musica.play(); 
-              boton.classList.remove("muted"); 
-          } else {
-
-              musica.pause(); 
-              boton.classList.add("muted");
-          }
+            boton.addEventListener("click", function () {
+                if (musica.paused) {
+                    musica.play();
+                    boton.classList.remove("muted");
+                } else {
+                    musica.pause();
+                    boton.classList.add("muted");
+                }
+            });
         });
-      });
     </script>
+
+</body>
+</html>
